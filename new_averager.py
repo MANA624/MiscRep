d8597a430e439770aaef69cc8dcfcbe5d018433c
Matt <pivotman624@gmail.com>
2016-06-28 20:45:12 -0600

    Finished all functionality. Just need to do testing and work out bugs and retouch code to put in portfolio

--- new_averager.py
+++ new_averager.py
@@ -1,11 +1,12 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
 from threading import Thread
+import shutil
 import csv
+import os
 
 # This is a comment!
-
-line = []
 
 
 class Gui:
@@ -16,8 +17,10 @@ class Gui:
     class_info = ""
     save_state = "disabled"
     categories = 0
-    line = []
     class_index = -1
+    line = []
+
+    child_window_open = False
 
     def __init__(self):
         Thread(target=self.read_classes).start()
@@ -43,7 +46,7 @@ class Gui:
         self.bottom_frame = Frame(self.root)
         self.bottom_frame.grid(row=self.active_boxes+2, columnspan=10)
 
-        self.reset = Button(self.bottom_frame, text="Reset", command=self.restart)
+        self.reset = Button(self.bottom_frame, text="Reset", command=lambda: self.restart(True))
         self.reset.grid(row=0, column=0, padx=10, pady=10)
 
         self.submit = Button(self.bottom_frame, text="Submit", command=lambda: Thread(target=self.submit_form).start())
@@ -51,9 +54,6 @@ class Gui:
 
         self.status = Label(self.bottom_frame)
         self.status.grid(padx=10, pady=10, row=1, columnspan=5)
-
-        self.test = Button(self.bottom_frame, text="Test it", command=self.test)
-        self.test.grid(row=2, columnspan=10, padx=10, pady=10)
 
         self.menu = Menu(self.root)
         self.root.config(menu=self.menu)
@@ -68,7 +68,7 @@ class Gui:
         self.file_menu.add_separator()
         self.file_menu.add_cascade(label='Load Class', menu=self.scores_load_menu)
 
-        self.save_command = self.file_menu.add_command(label='Save Scores', accelerator="Ctr+S", state=self.save_state)
+        self.save_command = self.file_menu.add_command(label='Save Scores', accelerator="Ctr+S", state=self.save_state, command=self.save_scores)
         self.file_menu.add_separator()
 
         self.file_menu.add_command(label='Quit', command=self.root.destroy)
@@ -83,21 +83,18 @@ class Gui:
         self.customize_menu.add_cascade(label="Delete Class", menu=self.delete_menu)
 
         for item in self.class_info:
-            self.naming.add_command(label=item[0], command=lambda: messagebox.showerror("rename that song"))
-            self.open_syl_menu.add_command(label=item[0], command=lambda: messagebox.showinfo("Yo", "Hey"))
+            self.naming.add_command(label=item[0], command=lambda label=item[0]: self.rename_class(label))
+            self.open_syl_menu.add_command(label=item[0], command=lambda label=item[0]: self.open_syl(label))
+            self.save_syl_menu.add_command(label=item[0], command=lambda label=item[0]: self.save_syl(label))
             self.scores_load_menu.add_command(label=item[0], command=lambda label=item[0]: self.load_class(label))
-            self.save_syl_menu.add_command(label=item[0], command=lambda: messagebox.showinfo("FDSJKF", "fdsfa"))
-            self.delete_menu.add_command(label=item[0], command=lambda row=item: self.delete_class(item))
+            self.delete_menu.add_command(label=item[0], command=lambda row=item: self.delete_class(row))
 
         self.help_menu = Menu(self.menu, tearoff=False)
         self.menu.add_cascade(label='Help', menu=self.help_menu)
-        self.help_menu.add_command(label='Help', command=lambda: messagebox.showerror("You need help"))
-        self.help_menu.add_command(label='About', command=lambda: messagebox.showerror("About that..."))
+        self.help_menu.add_command(label='Help', command=self.help)
+        self.help_menu.add_command(label='About', command=self.about)
 
         self.root.mainloop()
-
-    def test(self):
-        print("Tested")
 
     def read_classes(self):
         try:
@@ -111,7 +108,7 @@ class Gui:
     def fill_text(self):
         if self.class_index == -1:
             for row in range(self.active_boxes):
-                label = Label(self.root, text="Participation: ")
+                label = Label(self.root, text="")
                 label.grid(row=row+2, column=0, sticky=E, padx=10)
         else:
             for row in range(self.active_boxes):
@@ -120,7 +117,10 @@ class Gui:
 
     def create_entries(self, row, col):
         entry = Entry(self.root)
-        entry.insert(END, '0')
+        if self.class_index == -1:
+            entry.insert(END, '0')
+        else:
+            entry.insert(END, self.class_info[self.class_index][5*(row-2)+col+1])
         entry.grid(pady=10, column=col, row=row)
         entry.bind("<Return>", lambda _: Thread(target=self.submit_form).start())
         return entry
@@ -129,15 +129,15 @@ class Gui:
         var = IntVar()
         check_box = Checkbutton(self.root, variable=var)
         check_box.grid(padx=10, pady=10, column=col, row=row)
+        if self.class_index != -1 and self.class_info[self.class_index][5 * (row - 2) + col + 1] == "1":
+            check_box.select()
         return var
 
     def load_class(self, class_name):
         self.active_boxes = 6
         for i in range(len(self.class_info)):
             if self.class_info[i][0] == class_name:
-                self.status.config(text=class_name+" loaded!")
                 self.class_index = i
-                print(self.class_index)
                 break
         try:
             self.active_boxes = int((len(self.class_info[self.class_index])-1)/5)
@@ -145,7 +145,7 @@ class Gui:
             self.status.config(text="Class not found\nDid you corrupt the data file?")
 
         self.save_state = "normal"
-        self.restart()
+        self.restart(False)
 
     def submit_form(self):
 
@@ -182,7 +182,10 @@ class Gui:
 
         self.status.config(text="Your final grade is " + str(final_grade))
 
-    def restart(self, *message):
+    def restart(self, wipe):
+        if wipe:
+            self.class_index = -1
+            self.active_boxes = 6
         for child in self.root.winfo_children():
             child.destroy()
 
@@ -191,15 +194,29 @@ class Gui:
     def save_scores(self, *args):
         if self.save_state == "disabled":
             return
-        grades = [self.parse_grades(self.scores[box].get()) for box in range(self.active_boxes)]
-        weights = [self.parse_grades(self.weights[box].get()) for box in range(self.active_boxes)]
-        print(grades)
-        print(weights)
+        changed_line = [self.class_info[self.class_index][0]]
+
+        for item in range(self.active_boxes):
+            changed_line.extend((self.class_info[self.class_index][item*5+1], self.scores[item].get(), self.weights[item].get(), self.drop_low[item].get(), self.extra_credit[item].get()))
+
+        self.class_info[self.class_index] = changed_line
+
+        writing = open("Classes.csv", 'w', newline='')
+        writer = csv.writer(writing)
+        for row in self.class_info:
+            writer.writerow(row)
+        writing.close()
+        self.status.config(text="Saved!")
 
     def delete_class(self, row):
         if not messagebox.askokcancel("Careful!", "You are trying to delete " + row[0] +
                                       "\nAre you sure you want to continue?"):
             return
+
+        try:
+            os.remove(row[0] + ".pdf")
+        except FileNotFoundError:
+            pass
 
         if self.class_index != -1 and self.class_info[self.class_index] == row:
             self.active_boxes = 6
@@ -212,13 +229,19 @@ class Gui:
             writer.writerow(row)
         writing.close()
 
-        self.restart()
+        self.restart(False)
 
     def add_class(self, *args):
 
         if len(self.class_info) >= 20:
             self.status.config(text="Do you really need that many classes?")
             return
+
+        if self.child_window_open:
+            self.status.config(text="You already have one window open!")
+            return
+
+        self.child_window_open = True
 
         self.categories = 0
         self.line = []
@@ -230,6 +253,7 @@ class Gui:
                 title.config(text="What do you want to call your first catergory?")
             elif self.categories == 1:
                 title.config(text="What do you want to call your next category?")
+                instructions.config(text='Type "Quit" when done')
             if cat == "Quit":
                 if self.categories <= 1:
                     title.config(text="Oops! You need at least one category!")
@@ -240,7 +264,7 @@ class Gui:
                 writing.close()
                 root.destroy()
                 self.read_classes()
-                self.restart()
+                self.restart(False)
                 return
             if not cat:
                 title.config(text="You can't name it nothing!")
@@ -259,14 +283,17 @@ class Gui:
 
             if self.categories >= 15:
                 entry.insert(END, "Quit")
-                submit_info()
+                submit_info(self.line)
+            return self.line
 
         root = Tk()
+        root.title("Add a Class")
 
         title = Label(root, text="What is the name of your class to be called?")
         title.pack(padx=10, pady=10)
 
-        Label(root, text='Type "Quit" when done').pack(padx=10, pady=5)
+        instructions = Label(root)
+        instructions.pack(padx=10, pady=5)
 
         entry = Entry(root)
         entry.bind("<Return>", submit_info)
@@ -275,7 +302,65 @@ class Gui:
         button = Button(root, text="Submit", command=submit_info)
         button.pack()
 
+        root.protocol("WM_DELETE_WINDOW", lambda: self.close(root))
         root.mainloop()
+
+    def rename_class(self, to_be_renamed):
+        if self.child_window_open:
+            self.status.config(text="You already have one window open!")
+            return
+
+        self.child_window_open = True
+
+        for item in range(len(self.class_info)):
+            if self.class_info[item][0] == to_be_renamed:
+                temp_index = item
+                break
+
+        def submit_name(*args):
+            name = entry.get().title()
+            if len(name) > 12:
+                name = name[:12] + "..."
+
+            for item in self.class_info:
+                if item[0] == name:
+                    title.config(text="You already have a class with that name!")
+                    return
+
+            self.class_info[temp_index][0] = name
+
+            writing = open("Classes.csv", 'w', newline='')
+            writer = csv.writer(writing)
+            for line in self.class_info:
+                writer.writerow(line)
+            writing.close()
+
+            try:
+                os.rename(to_be_renamed+'.pdf', name+'.pdf')
+            except FileNotFoundError:
+                pass
+
+            root.destroy()
+            self.restart(False)
+
+        root = Tk()
+        root.title("Rename a Class")
+
+        title = Label(root, text="What do you want your class to be renamed to?")
+        title.pack(padx=10, pady=10)
+
+        entry = Entry(root)
+        entry.bind("<Return>", submit_name)
+        entry.pack(padx=10, pady=10)
+
+        Button(root, text="Submit", command=submit_name).pack(padx=10, pady=10)
+
+        root.protocol("WM_DELETE_WINDOW", lambda: self.close(root))
+        root.mainloop()
+
+    def close(self, root):
+        self.child_window_open = False
+        root.destroy()
 
     def parse_grades(self, grades, drop):
         scores = grades.split(',')
@@ -295,6 +380,105 @@ class Gui:
         avg = sum(scores) / len(scores)
 
         return avg
+
+    def save_syl(self, class_name):
+        syl_name = askopenfilename(filetypes=(("PDF files", "*.pdf;*.PDF"),
+                                              ("All files", "*.*")))
+
+        if not syl_name:
+            return
+
+        if not syl_name.lower().endswith(".pdf"):
+            self.status.config(text="That's not a PDF file, sorry")
+            return
+
+        shutil.copyfile(syl_name, class_name + '.pdf')
+
+    def open_syl(self, class_name):
+        try:
+            os.startfile(class_name + '.pdf')
+        except FileNotFoundError:
+            self.status.config(text="No syllabus for this class yet!")
+
+    def help(self):
+        if self.child_window_open:
+            self.status.config(text="You already have one window open!")
+            return
+        self.child_window_open = True
+
+        def config_text(*args):
+            if var.get() == 'General':
+                label.config(text=gen_text)
+
+            if var.get() == 'Renaming Classes':
+                label.config(text=rnme_text)
+
+            if var.get() == 'Opening Your Syllabus':
+                label.config(text=syl_text)
+
+            if var.get() == 'Opening Saved Grades':
+                label.config(text=grd_text)
+        root = Tk()
+        root.minsize(600, 200)
+        root.maxsize(600, 200)
+        root.title("Help")
+
+        var = StringVar(root)
+        # initial value
+        var.set('General')
+        choices = ['General', 'Renaming Classes', 'Opening Your Syllabus', 'Opening Saved Grades']
+        option = OptionMenu(root, var, *choices, command=config_text)
+
+        option.grid(pady=20, padx=10, sticky=S)
+
+        root.title('Help')
+
+        gen_text = "Welcome to the Grade Averager!\n\n" \
+                   "In the grade percentage, enter as many grades as you want in the\n" \
+                   "same box separated by commas and they will be averaged. Then put the\n" \
+                   "percentage of weight they have and enter it. Feel free to leave any boxes\n" \
+                   "Please feel free to give suggestions and comments!"
+
+        rnme_text = "In order to rename a class go to Customize > Rename Class and\n" \
+                    "then pick the class you want to rename. Type what you want to\n" \
+                    "rename it to and press Enter. It really is that easy"
+
+        syl_text = "It is now easier than ever to keep track of syllabi! Just go to\n" \
+                   "File > Save Syllabus and click on the appropriate class, then find\n" \
+                   "the syllabus. When you select it, the program will save it. Then\n" \
+                   "next time you want to open your syllabus just go to File > Open\n" \
+                   "and it will open in your default .pdf reader"
+
+        grd_text = "In order to save a class you must first load a class by going\n" \
+                   "to File > Load Class, and loading a created class. Then when you\n" \
+                   "go to File > Save Class it will save all the data and options for\n" \
+                   "the class"
+
+        label = Label(root, text=gen_text)
+        label.grid(row=0, column=1, rowspan=2, padx=30, pady=30, sticky=N)
+
+        root.protocol("WM_DELETE_WINDOW", lambda: self.close(root))
+        root.mainloop()
+
+    def about(self):
+        if self.child_window_open:
+            self.status.config(text="You already have one window open!")
+            return
+        self.child_window_open = True
+
+        root = Tk()
+        root.title('About this program')
+
+        label = Label(root, text="This is a privately developed Python GUI Application\n"
+                                  "developed soley by Matthew Niemiec. Feel free to use\n"
+                                  "it as needed, just don't do anything stupid like showing\n"
+                                  "it to your friends and saying it's yours. That's just\n"
+                                  "rude. Special thanks to Adam Smith for critiques and\n"
+                                  "general guidance. You're the coolest, Adam!")
+        label.pack(side=TOP, padx=30, pady=30)
+
+        root.protocol("WM_DELETE_WINDOW", lambda: self.close(root))
+        root.mainloop()
 
 
 def main():
